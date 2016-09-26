83450ad8af3cbb3462862c2e60a0fbaf2d99bbd9
David Kent <davidkent@fastmail.com.au>
2016-09-26 22:15:23 +1000

    Some basic e2e tests for the client
    Based off some data provided by the API from the base.yaml fixture.

---
+++ src/client/app/accounts.component.e2e-spec.ts
@@ -0,0 +1,20 @@
+describe('Accounts', () => {
+
+  beforeEach( () => {
+    browser.get('/accounts');
+  });
+
+  it('should have a list of accounts', () => {
+    expect(element(by.css('account-list ul')).getText())
+      .toEqual('Statement Account\nSavings\nCredit Card');
+  });
+
+  it('should show detail form on click', () => {
+    element(by.css('account-list ul li:first-child')).click();
+
+    expect(element(by.css('account-list account-detail')).isPresent());
+    expect(element(by.css('account-list account-detail div h2')).getText())
+      .toEqual('Statement Account');
+  });
+
+});
--- src/client/app/app.component.e2e-spec.ts
+++ src/client/app/app.component.e2e-spec.ts
@@ -5,19 +5,23 @@ describe('App', () => {
   });
 
   it('should have a title', () => {
-    expect(browser.getTitle()).toEqual('Welcome to angular2-seed!');
+    expect(browser.getTitle()).toEqual('CatTrack');
   });
 
   it('should have <nav>', () => {
-    expect(element(by.css('sd-app sd-navbar nav')).isPresent()).toEqual(true);
+    expect(element(by.css('sd-app nav')).isPresent()).toEqual(true);
   });
 
-  it('should have correct nav text for Home', () => {
-    expect(element(by.css('sd-app sd-navbar nav a:first-child')).getText()).toEqual('HOME');
+  it('should have correct nav text for Dashboard', () => {
+    expect(element(by.css('sd-app nav div ul li:nth-child(1) a')).getText()).toEqual('Dashboard');
   });
 
-  it('should have correct nav text for About', () => {
-    expect(element(by.css('sd-app sd-navbar nav a:last-child')).getText()).toEqual('ABOUT');
+  it('should have correct nav text for Transactions', () => {
+    expect(element(by.css('sd-app nav div ul li:nth-child(2) a')).getText()).toEqual('Accounts');
+  });
+
+  it('should have correct nav text for Transactions', () => {
+    expect(element(by.css('sd-app nav div ul li:nth-child(3) a')).getText()).toEqual('Transactions');
   });
 
 });
